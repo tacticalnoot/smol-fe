5b3ab7206da91c599e0a7dc4fd25f05d2beb80df
kalepail <hi@tyvdh.com>
2025-02-25 16:18:19 -0700

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: cloudflare()
 });
