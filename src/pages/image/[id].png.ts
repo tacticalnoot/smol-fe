37cc53007afabfcf1dbe19d7ed5ac218cbc1c247
kalepail <hi@tyvdh.com>
2025-04-16 16:36:46 -0400

    attempt to use jimp

--- src/pages/image/[id].png.ts
+++ src/pages/image/[id].png.ts
@@ -1,9 +1,11 @@
 import type { APIRoute } from "astro";
 import { Buffer } from "buffer";
+import { Jimp, ResizeStrategy } from "jimp";
 
 // TODO this is a very bad way to do this, but it works for now
 
-export const GET: APIRoute = async ({ params }) => {
+export const GET: APIRoute = async ({ params, url }) => {
+	const scale = url.searchParams.get('scale');
 	const id = params.id;
 	const data = id
 		? await fetch(
@@ -14,7 +16,21 @@ export const GET: APIRoute = async ({ params }) => {
 		})
 		: null;
 
-	return new Response(Buffer.from(data.do.image_base64, 'base64'), {
+	let img: Buffer = Buffer.from(data.do.image_base64, 'base64');
+
+	if (scale) {
+		const image = await Jimp.fromBuffer(img);
+
+		image.resize({
+			w: image.width * parseInt(scale),
+			h: image.height * parseInt(scale),
+			mode: ResizeStrategy.NEAREST_NEIGHBOR,
+		});
+
+		img = await image.getBuffer('image/png')
+	}
+
+	return new Response(img, {
 		status: 200,
 		headers: {
 			"Content-Type": "image/png",
