88895307545c85bbaa2a1e9bde7f054a5f36f7e9
kalepail <hi@tyvdh.com>
2025-04-28 22:47:10 -0400

    shrug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
+// import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import mkcert from 'vite-plugin-mkcert';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,17 +13,23 @@ export default defineConfig({
   integrations: [svelte()],
   adapter: cloudflare(),
   vite: {
-    optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: 'globalThis',
-        },
-        plugins: [
-          nodeModulesPolyfillPlugin()
-        ]
-      }
+    // optimizeDeps: {
+    //   esbuildOptions: {
+    //     define: {
+    //       global: 'globalThis',
+    //     },
+    //     plugins: [
+    //       nodeModulesPolyfillPlugin()
+    //     ]
+    //   }
+    // },
+    define: {
+      "process.env": process.env
     },
     plugins: [
+      nodePolyfills({
+        include: ['buffer']
+      }),
       mkcert(),
       tailwindcss()
     ],
