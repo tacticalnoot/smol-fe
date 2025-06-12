de8ed658e4e7c65f4b2f0f16ff3a2dfd130b1aa0
kalepail <hi@tyvdh.com>
2025-06-12 13:13:29 -0400

    add like button to smol page

--- src/components/Smol.svelte
+++ src/components/Smol.svelte
@@ -5,6 +5,11 @@
     import { onDestroy, onMount } from "svelte";
     import Loader from "./Loader.svelte";
     import { contractId } from "../store/contractId";
+    import { Address, hash, StrKey, xdr } from "@stellar/stellar-sdk/minimal";
+    import { Client } from "fp-sdk";
+    import { publicKey, rpc } from "../utils/base";
+    import { account } from "../utils/passkey-kit";
+    import { keyId } from "../store/keyId";
 
     let d1 = data?.d1;
     let kv_do = data?.kv_do;
@@ -234,6 +239,73 @@
             });
     }
 
+    async function songLike() {
+        if (!d1?.Id) return;
+        try {
+            d1.Liking = true;
+            let xdr_string: string | undefined = undefined;
+            if (d1.Liked) {
+                if (!$contractId || !$keyId) return;
+
+                const contract_id: string | undefined = StrKey.encodeContract(
+                    hash(
+                        xdr.HashIdPreimage.envelopeTypeContractId(
+                            new xdr.HashIdPreimageContractId({
+                                networkId: hash(
+                                    Buffer.from(
+                                        import.meta.env.PUBLIC_NETWORK_PASSPHRASE,
+                                    ),
+                                ),
+                                contractIdPreimage:
+                                    xdr.ContractIdPreimage.contractIdPreimageFromAddress(
+                                        new xdr.ContractIdPreimageFromAddress({
+                                            address: Address.fromString(publicKey).toScAddress(),
+                                            salt: Buffer.from(d1.Id, "hex"),
+                                        }),
+                                    ),
+                            }),
+                        ).toXDR(),
+                    ),
+                );
+
+                const contract = new Client({
+                    contractId: contract_id,
+                    rpcUrl: import.meta.env.PUBLIC_RPC_URL,
+                    networkPassphrase: import.meta.env.PUBLIC_NETWORK_PASSPHRASE,
+                });
+
+                let at = await contract.burn({
+                    from: $contractId,
+                    amount: 1n,
+                });
+
+                const { sequence } = await rpc.getLatestLedger();
+                at = await account.sign(at, {
+                    keyId: $keyId,
+                    expiration: sequence + 60,
+                });
+                xdr_string = at.built?.toXDR();
+            }
+
+            await fetch(`${import.meta.env.PUBLIC_API_URL}/like/${d1.Id}`, {
+                method: "PUT",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    xdr: xdr_string,
+                }),
+            }).then(async (res) => {
+                if (!res.ok) throw await res.text();
+            });
+
+            d1.Liked = !d1.Liked;
+        } finally {
+            d1.Liking = false;
+        }
+    }
+
     // Reactive statement to apply length limit when is_instrumental changes
     $: if (is_instrumental || !is_instrumental) {
         limitPromptLength();
@@ -426,6 +498,45 @@
                                         clip-rule="evenodd"
                                     />
                                 </svg>
+                            </button>
+                        {/if}
+
+                        {#if $contractId}
+                            <button
+                                class="p-2 rounded-lg backdrop-blur-xs hover:bg-slate-950/70 transition-colors"
+                                aria-label="Like"
+                                disabled={d1?.Liking}
+                                on:click={songLike}
+                            >
+                                {#if d1?.Liking}
+                                    <Loader classNames="w-5 h-5" />
+                                {:else if d1?.Liked}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                        class="size-5"
+                                    >
+                                        <path
+                                            d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z"
+                                        />
+                                    </svg>
+                                {:else}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        class="size-5"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                                        />
+                                    </svg>
+                                {/if}
                             </button>
                         {/if}
                     {/if}
