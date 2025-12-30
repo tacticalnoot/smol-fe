435b5fe4d2f674f9624d9a41e8c28e9523ffc503
tacticalnoot <jeff@noot.xlm>
2025-12-30 16:16:18 -0600

    fix(radio): default to builder view on navigation

--- src/components/radio/RadioBuilder.svelte
+++ src/components/radio/RadioBuilder.svelte
@@ -165,7 +165,7 @@
         if (state.isActiveGlobalShuffle !== undefined)
           isActiveGlobalShuffle = state.isActiveGlobalShuffle;
         if (state.currentIndex !== undefined) currentIndex = state.currentIndex;
-        if (state.showBuilder !== undefined) showBuilder = state.showBuilder;
+        if (state.currentIndex !== undefined) currentIndex = state.currentIndex;
 
         // Reset history set from loaded IDs
         if (state.generatedPlaylist) {
